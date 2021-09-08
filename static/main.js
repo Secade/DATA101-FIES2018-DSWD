ae0d680e6844a6697fc787d671889920c88e3783
Secade <47266084+Secade@users.noreply.github.com>
2021-09-08 19:09:39 +0800

    Added graph connection to filter, also changes on zoom level however it doesnt change automatically on zoom, need to change to another filter first

--- static/main.js
+++ static/main.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
     mapboxgl.accessToken = 'pk.eyJ1Ijoic2VjYWRlIiwiYSI6ImNrcnU3cHVnNDNvZHQycHRqZnZnNzQxYXQifQ.F-OV6UiB-D5fepALN_4stA';
 
-    var countryselect = $("#countryselect");
+    var filterselect = $("#filterselect");
+    var currentLevel = 'region';
 
-    d3.json('/filters').then(function(data){
+    d3.json('/filters').then(function (data) {
         console.log(data)
-        data.forEach(function(elem,i){
-          countryselect.append('<option value="'+elem['0']+'">'+elem['0']+'</option>');
-    
+        data.forEach(function (elem, i) {
+            filterselect.append('<option value="' + elem['0'] + '">' + elem['0'] + '</option>');
+
         });
-      });
+    });
 
 
     const bounds = [
@@ -48,7 +49,7 @@ $(document).ready(function () {
             url: 'mapbox://secade.6v2gs6xf',
         });
 
-        
+
 
         map.addLayer({
             id: 'prov',
@@ -121,7 +122,159 @@ $(document).ready(function () {
                     .addTo(map);
             });
         });
+
+        map.on('zoom', () => {
+            if (map.getZoom() > 7) {
+            currentLevel = 'province'
+            } else {
+            currentLevel = 'region'
+            }
+            });
+
+
+    });
+
+    var w = 600;
+    var h = 500;
+    var padding_left = 100;
+    var padding = 50;
+
+    var bar = d3.select("#bar").append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    var maxRatio, regions,
+        xScale, yScale,
+        xAxis,
+        yAxis;
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    d3.json('/filters/Meat').then(function (data) {
+        data.sort(function(a,b) { return b.filter - a.filter;});
+
+        if(currentLevel=='province'){
+            data=data.slice(0,10);
+        }
+
+        maxRatio = d3.max(data, function (d) { return d.filter; });
+
+        regions = data.map(function (d) { return eval("d."+currentLevel); });
+
+        xScale = d3.scaleLinear([0, maxRatio], [padding, w - padding_left]);
+
+        yScale = d3.scaleBand()
+            .domain(regions)
+            .rangeRound([padding, h - padding])
+            .padding(0.1);
+
+        var xAxis = d3.axisBottom(xScale);
+        var yAxis = d3.axisLeft(yScale);
+
+        bar.append("g")
+            .attr("id", "xAxis")
+            .attr("transform", "translate(" + (padding_left - padding) + "," + (h - padding) + ")")
+            .call(xAxis);
+
+        bar.append("g")
+            .attr("id", "yAxis")
+            .attr("transform", "translate(" + (padding_left) + ",0)")
+            .call(yAxis);
+
+        bar.append("text")
+            .attr("id", "tableText")
+            .attr("x", w / 2)
+            .attr("y", h - padding + 40)
+            .attr("text-anchor", "middle")
+            .text(" Spending (Pesos)");
+
+        bar.selectAll("rect")
+            .data(data)
+            .join("rect")
+            .attr("x", padding_left + 1)
+            .attr("y", d => yScale(eval("d."+currentLevel)))
+            .attr("width", 0)
+            .attr("height", d => yScale.bandwidth())
+            .style("fill", "darkblue")
+
+    });
+
+    $("#filterselect").on("change", function (event) {
+        // console.log(event);
+        var selected_filter = $("#filterselect").val();
+
+        var t = bar.transition()
+            .duration(1000);
+
+        //console.log(selected_country);
+        d3.json('/filters/' + selected_filter).then(function (data) {
+            data.sort(function(a,b) { return b.filter - a.filter;});
+
+            if(currentLevel=='province'){
+                data=data.slice(0,10);
+            }
+
+            maxRatio = d3.max(data, function (d) { return d.filter; });
+
+            regions = data.map(function (d) { return eval("d."+currentLevel); });
+
+            xScale = d3.scaleLinear([0, maxRatio], [padding, w - padding_left]);
+
+            yScale = d3.scaleBand()
+                .domain(regions)
+                .rangeRound([padding, h - padding])
+                .padding(0.1);
+
+            var xAxis = d3.axisBottom(xScale);
+            var yAxis = d3.axisLeft(yScale);
+
+            bar.select("#xAxis")
+                .call(function (update) {
+                    update.transition(t)
+                        .call(xAxis);
+                });
+
+            bar.select("#yAxis")
+                .call(function (update) {
+                    update.transition(t)
+                        .call(yAxis);
+                });
+
+              bar.select("#tableText").text(selected_filter+" Spending (Pesos)");
+              
+            bar.selectAll("rect")
+                .data(data)
+                .join(function (enter) {
+                    enter.append("rect")
+                        .attr("x", padding_left + 1)
+                        .attr("y", d => yScale(eval("d."+currentLevel)))
+                        .attr("width", d => xScale(d.filter))
+                        .attr("height", d => yScale.bandwidth())
+                        .style("fill", "darkblue");
+                }, function (update) {
+                    update.call(function (update) {
+                        update.transition(t)
+                            .attr("width", d => xScale(d.filter))
+                            .attr("height", d => yScale.bandwidth())
+                            .style("fill", "darkblue");
+                    })
+                }, function (exit) {
+                    exit.attr("fill", "#cccccc")
+                        .call(function (exit) {
+                            exit.transition(t)
+                                .attr("width", d => xScale(0))
+                                .attr("height", d => yScale(0))
+                                .remove();
+                        })
+                }
+                )
+        });
     });
 });
-
-
